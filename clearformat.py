392b690bf4f4fea941862dca64f0e3e44ddea3cb
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2022-06-26 08:39:18 +0700

    Update clearformat.py

--- clearformat.py
+++ clearformat.py
@@ -1,30 +1,30 @@
 # ************************** man hinh loai 2 *************************
 import sys
+# pip install pyqt6
 from PyQt5 import QtCore
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit
-
 from gui import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.a = None
         self.uic = Ui_MainWindow()
         self.uic.setupUi(self)
         self.textedit = QTextEdit()
         self.uic.verticalLayout_2.addWidget(self.textedit)
-        self.uic.pushButton.clicked.connect(self.correct)
 
-    def correct(self):
+        self.uic.pushButton.clicked.connect(self.format_font)
+
+    def format_font(self):
+        a = self.textedit.toPlainText()
+        self.textedit.setText(a)
         self.textedit.selectAll()
-        # self.textedit.setCurrentCharFormat(QtGui.QTextCharFormat())
-
-        # cursor = self.textedit.textCursor()
-        # # cursor.select(QtGui.QTextCursor.Document)
-        # cursor.setCharFormat(QtGui.QTextCharFormat())
-        # cursor.clearSelection()
-        self.textedit.setAlignment(QtCore.Qt.AlignCenter)
+        self.textedit.setAlignment(QtCore.Qt.AlignHCenter)
+        font = QFont()
+        font.setPointSize(20)
+        self.textedit.setFont(font)
 
 
 if __name__ == "__main__":
